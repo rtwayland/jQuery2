609daf026e187b1ed5b55cf6f54ff59effe2c23b
raleigh wayland <rtwayland@gmail.com>
2017-01-12 15:05:24 -0700

    Tasks stored and retrieved

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    var listo = [];
+    var sessionContents = sessionStorage.getItem("tasks");
+    if (sessionContents !== null) {
+        var listo = JSON.parse(sessionContents);
+        placeTasks(listo);
+    } else {
+        var listo = [];
+    }
     $('#newTaskForm').hide();
 
     function Task(task) {
@@ -7,10 +13,61 @@ $(document).ready(function() {
         this.id = 'new';
     }
 
+    function placeTasks(listo) {
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].id === 'new') {
+                addNew(listo[i]);
+            } else if (listo[i].id === 'inProgress') {
+                addProgress(listo[i]);
+            } else {
+                addArchive(listo[i]);
+            }
+        }
+    }
+
+    function addNew(task) {
+        $('#newList').append(
+            '<a href="#finish" class="" id="item">' +
+            '<li class="list-group-item">' +
+            '<h3>' + task.task + '</h3>' +
+            '<span class="arrow pull-right">' +
+            '<i class="glyphicon glyphicon-arrow-right">' +
+            '</span>' +
+            '</li>' +
+            '</a>'
+        );
+    }
+
+    function addProgress(task) {
+        $('#currentList').append(
+            '<a href="#finish" class="" id="inProgress">' +
+            '<li class="list-group-item">' +
+            '<h3>' + task.task + '</h3>' +
+            '<span class="arrow pull-right">' +
+            '<i class="glyphicon glyphicon-arrow-right">' +
+            '</span>' +
+            '</li>' +
+            '</a>'
+        );
+    }
+
+    function addArchive(task) {
+        $('#archivedList').append(
+            '<a href="#finish" class="" id="archived">' +
+            '<li class="list-group-item">' +
+            '<h3>' + task.task + '</h3>' +
+            '<span class="arrow pull-right">' +
+            '<i class="glyphicon glyphicon-remove">' +
+            '</span>' +
+            '</li>' +
+            '</a>'
+        );
+    }
+
     var advanceTask = function(task) {
         var modified = task.innerText.trim()
         for (var i = 0; i < listo.length; i++) {
-            if (listo[i].task === modified) {
+            if (listo[i].task.toUpperCase() === modified) {
                 if (listo[i].id === 'new') {
                     listo[i].id = 'inProgress';
                 } else if (listo[i].id === 'inProgress') {
@@ -29,17 +86,10 @@ $(document).ready(function() {
             task = new Task(task);
             listo.push(task);
 
+            sessionStorage.setItem("tasks", JSON.stringify(listo));
+
             $('#newItemInput').val('');
-            $('#newList').append(
-                '<a href="#finish" class="" id="item">' +
-                '<li class="list-group-item">' +
-                '<h3>' + task.task + '</h3>' +
-                '<span class="arrow pull-right">' +
-                '<i class="glyphicon glyphicon-arrow-right">' +
-                '</span>' +
-                '</li>' +
-                '</a>'
-            );
+            addNew(task);
         }
         $('#newTaskForm').slideToggle('fast', 'linear');
     }
@@ -65,20 +115,26 @@ $(document).ready(function() {
         advanceTask(task);
         this.id = 'inProgress';
         $('#currentList').append(this.outerHTML);
+
+        sessionStorage.setItem("tasks", JSON.stringify(listo));
     });
 
     $(document).on('click', '#inProgress', function(e) {
         e.preventDefault();
         var task = this;
+        advanceTask(task);
         task.id = "archived";
         var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
-        advanceTask(task);
         $('#archivedList').append(changeIcon);
+
+        sessionStorage.setItem("tasks", JSON.stringify(listo));
     });
 
     $(document).on('click', '#archived', function(e) {
         e.preventDefault();
         var task = this;
         advanceTask(task);
+
+        sessionStorage.setItem("tasks", JSON.stringify(listo));
     });
 });
